3f2e3606eca04b831a9befa2462160bb9df7653c
Sebastien Martinez <sebmar.8128@gmail.com>
2026-04-25 21:22:11 -0400

    create default settings header

---
+++ inc/default_settings.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <Arduino.h>
+#include <string.h>
+
+#include "types.h"
+
+namespace DefaultSettings {
+
+static constexpr uint32_t GAME_SEED = 0x41802026;
+
+inline UserSettings userSettings() {
+    UserSettings settings = {};
+
+    strncpy(settings.username, "PLAYER", MAX_USERNAME_LEN);
+    settings.username[MAX_USERNAME_LEN] = '\0';
+
+    settings.buttonMapping = {
+        PhysicalButton::LdLeft,
+        PhysicalButton::LdRight,
+        PhysicalButton::LdDown,
+        PhysicalButton::LdUp,
+        PhysicalButton::RdUp,
+        PhysicalButton::RdLeft,
+        PhysicalButton::RdDown
+    };
+    settings.theme = ThemeId::Modern;
+    settings.holdEnabled = true;
+    settings.ghostEnabled = true;
+    settings.nextPreviewCount = MAX_NEXT_PIECES;
+
+    return settings;
+}
+
+inline MatchSettings matchSettings() {
+    MatchSettings settings = {};
+    settings.garbageEnabled = false;
+    settings.mode = GameMode::Marathon;
+    settings.startingLevel = 0;
+    return settings;
+}
+
+}  // namespace DefaultSettings
